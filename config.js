4e2fc698848e6d6696eb0b1b8b025622bd07843f
Elliuuren <elliuur3n@gmail.com>
2026-05-23 16:31:13 +0600

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ==============================
 
 const SUPABASE_URL = 'https://lusezbemprwfapqovgus.supabase.co';
-const SUPABASE_KEY = 'YOUR_ANON_PUBLIC_KEY';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1c2V6YmVtcHJ3ZmFwcW92Z3VzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1MjM5NzIsImV4cCI6MjA5NTA5OTk3Mn0.a8VupRisEkFfUaqiHRZi75prDHNBpOBEuVzmtWQhTZY';
 
 // Admin password — change this to whatever you want
 const ADMIN_PASSWORD = 'orizeen2025';
